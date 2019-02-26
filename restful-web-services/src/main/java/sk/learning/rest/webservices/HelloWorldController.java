20e357f2b67b68849e4e5782b93ab130755374a8
SelvaKumar T <selvamailz@gmail.com>
2019-02-26 11:12:39 +0530

    First Commit

---
+++ restful-web-services/src/main/java/sk/learning/rest/webservices/HelloWorldController.java
@@ -0,0 +1,24 @@
+package sk.learning.rest.webservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String SayHelloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean SayHelloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/{name}")
+	public HelloWorldBean SayHelloBeanVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello " + name);
+	}
+}
